98de35da8f4fecd53a704eafc05ad6fdb6a845b3
Rafael Benevides <rafabenevides@pop-os.localdomain>
2022-05-29 16:32:42 -0300

    Init commit

---
+++ basecamp-javascript/sintaxe-operadores/compararNumeros.js
@@ -0,0 +1,10 @@
+function compararNumeros(a, b) {
+  if (a === b && a + b > 10 && a + b > 20) {
+    console.log(
+      `Os números ${a} e ${b} são iguais. Sua soma é ${
+        a + b
+      } é maior que 10 e maior que 20.`
+    )
+  }
+}
+compararNumeros(25, 25)
