273f7db3bc8d3a3ec594db2014ab63ac60efd8a9
Rafael Benevides <rafabenevides@pop-os.localdomain>
2022-05-30 15:46:25 -0300

    Resolução da atividade.

--- basecamp-javascript/sintaxe-operadores/compararNumeros.js
+++ basecamp-javascript/sintaxe-operadores/compararNumeros.js
@@ -1,10 +1,38 @@
-function compararNumeros(a, b) {
-  if (a === b && a + b > 10 && a + b > 20) {
-    console.log(
-      `Os números ${a} e ${b} são iguais. Sua soma é ${
-        a + b
-      } é maior que 10 e maior que 20.`
-    )
-  }
+function compararNumeros(num1, num2) {
+  const primeiraFrase = criaPrimeiraFrase(num1, num2)
+  const segundaFrase = criaSegundaFrase(num1, num2)
+  return `${primeiraFrase} ${segundaFrase}`
 }
-compararNumeros(25, 25)
+
+function criaPrimeiraFrase(num1, num2) {
+  let primeiraFrase = `Os números ${num1} e ${num2}`
+  let nao = ''
+
+  if (num1 !== num2) {
+    nao = 'não'
+  }
+
+  return `${primeiraFrase} ${nao} são iguais.`
+}
+
+function criaSegundaFrase(num1, num2) {
+  const soma = num1 + num2
+  let comparaDez = 'menor do que'
+  let comparaVinte = 'menor do que'
+
+  if (soma > 10) {
+    comparaDez = 'maior do que'
+  } else if (soma === 10) {
+    comparaDez = 'igual a'
+  }
+
+  if (soma > 20) {
+    comparaVinte = 'maior do que'
+  } else if (soma === 20) {
+    comparaVinte = 'igual a'
+  }
+
+  return `Sua soma é ${soma}, que é ${comparaDez} 10 e ${comparaVinte} 20.`
+}
+
+console.log(compararNumeros(5, 5))
